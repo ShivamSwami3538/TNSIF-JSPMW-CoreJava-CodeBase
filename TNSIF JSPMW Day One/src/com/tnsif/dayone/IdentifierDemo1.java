c978172fd3e4c4116875b5da23d1be115513d706
SHIVAM <ShivamSwami3538>
2025-08-04 19:15:47 +0530

    String, Value Declaration, Value Initialization, datatype's study done successfully.

---
+++ TNSIF JSPMW Day One/src/com/tnsif/dayone/IdentifierDemo1.java
@@ -0,0 +1,29 @@
+package com.tnsif.dayone;
+
+public class IdentifierDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Variables declaration valid / invalid
+		
+//		int for = 13;
+//		int num1 = 13;
+//		@num
+		
+		int $num1;       // Value Declaration
+		int $num =12;    // Value Initialization
+		int sum = $num;
+
+		System.out.println("Value of the number variable is: " + sum);
+//		System.out.println("Value of the number variable is: " + $num);
+		
+		String studentName = "Shreya";
+		System.out.println("Value of the name is: " + studentName);
+		
+//		Variable Declare 
+		int age;    // Value Declare
+		age = 20;
+		System.out.println("Age of the Boy is: " + age);
+	}
+
+}
