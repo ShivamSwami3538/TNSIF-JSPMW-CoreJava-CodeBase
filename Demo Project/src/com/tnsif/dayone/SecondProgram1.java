caaea01e465e0c0a73e06e698a452035a3476ec4
SHIVAM <ShivamSwami3538>
2025-08-04 18:45:09 +0530

    Demo Program Practice

---
+++ Demo Project/src/com/tnsif/dayone/SecondProgram1.java
@@ -0,0 +1,10 @@
+package com.tnsif.dayone;
+
+public class SecondProgram1 {
+
+	public static void main(String[] args) {
+				System.out.println("----My Second Demo Program----");
+				System.out.println("-------TNSIF Training in JSPM------");
+	}
+
+}
