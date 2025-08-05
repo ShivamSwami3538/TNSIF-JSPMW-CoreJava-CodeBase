3f59f12a2d83f517ee575bfb4785125735a06c25
SHIVAM <ShivamSwami3538>
2025-08-05 09:46:01 +0530

    Decision making statements If else practice program run successfully.

---
+++ TNSIF Day Three3/src/com/tnsif/daythree/Ifelsedemo.java
@@ -0,0 +1,23 @@
+package com.tnsif.daythree;
+
+public class Ifelsedemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 2;
+		if(age >= 18) {
+			System.out.println("You are eligible for Voting.");
+		} else {
+			System.out.println("Sorry! You have to wait for age 18 to get eligibility for voting.");
+		}
+		
+		int age1 = 22;
+		if(age1 >= 18) {
+			System.out.println("You are eligible for Voting.");
+		} else {
+			System.out.println("Sorry! You have to wait for age 18 to get eligibility for voting.");
+		}
+
+	}
+
+}
