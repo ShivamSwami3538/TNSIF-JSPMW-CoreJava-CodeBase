2d5214dbdc26fae3da3c0661919f3e5a5c1e67e1
SHIVAM <ShivamSwami3538>
2025-08-05 09:12:36 +0530

    Typecasting program Done Successfully.

---
+++ TNSIF Day Three1/src/com/tnsif/daythree/Typecastingdemo.java
@@ -0,0 +1,35 @@
+package com.tnsif.daythree;
+
+public class Typecastingdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// widening    // implicit typecasting
+		 byte b = 10;
+		 int l = b;
+		 System.out.println(l);
+
+		 float F = 22.14F;
+		 double d = F;
+		 System.out.println(d);
+		 
+		 char ch = 'A';
+		 int i = ch;
+		 System.out.println(i);
+		 
+		 // Narrowing datatype  // explicit typecasting 
+		 double F1 = 10.52F;
+		 long ll = (long) F1;
+		 System.out.println(ll);
+		 
+		 int i2 = 130;
+		 byte b1 = (byte) i2;
+		 System.out.println(b1);
+		 
+		 long i3 = 8668525992l;
+		 int b2 = (int) i3;
+		 System.out.println(b2);
+	
+	}
+}
+
