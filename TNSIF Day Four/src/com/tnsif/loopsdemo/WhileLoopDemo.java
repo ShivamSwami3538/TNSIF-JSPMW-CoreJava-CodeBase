77343d3f7cafb8ff9a33ac245cc8942b6245cdf2
SHIVAM <ShivamSwami3538>
2025-08-05 18:56:44 +0530

    Two Loop Statements are completed For Loop & While Loop Program Run Successfully.

---
+++ TNSIF Day Four/src/com/tnsif/loopsdemo/WhileLoopDemo.java
@@ -0,0 +1,31 @@
+package com.tnsif.loopsdemo;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int userInput = 91;
+		while(userInput<=100)
+		{
+			System.out.println(userInput);
+			userInput++;
+		}
+		
+		int userInput1 = 100;
+		while(userInput1<=100)
+		{
+			System.out.println(userInput1);
+			userInput1++;
+		}
+		
+//		No Output
+//		int userInput2 = 101;
+//		while(userInput2<=100)
+//		{
+//			System.out.println(userInput2);
+//			userInput2++;
+//		}
+		
+	}
+
+}
