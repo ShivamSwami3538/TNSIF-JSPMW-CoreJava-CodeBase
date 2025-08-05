ab131508fc74395f57417e1de55326fa2484d220
SHIVAM <ShivamSwami3538>
2025-08-05 19:01:25 +0530

    Loop Statement completed successfully program run Successfully.

---
+++ TNSIF Day Four/src/com/tnsif/loopsdemo/DoWhileLoopDemo.java
@@ -0,0 +1,24 @@
+package com.tnsif.loopsdemo;
+
+public class DoWhileLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=11;
+		do 
+		{
+			System.out.println(i);
+			i++;
+		}
+		while(i<=10);
+		
+		int i1=6;
+		do 
+		{
+			System.out.println(i1);
+			i1++;
+		}
+		while(i1<=10);
+	}
+
+}
