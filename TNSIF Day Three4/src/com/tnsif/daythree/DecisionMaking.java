5c2517d4cff73f5cf25bbd8ad14a8bbf2cb52642
SHIVAM <ShivamSwami3538>
2025-08-05 09:57:35 +0530

    Decision Making if else practice done successfully.

---
+++ TNSIF Day Three4/src/com/tnsif/daythree/DecisionMaking.java
@@ -0,0 +1,25 @@
+package com.tnsif.daythree;
+
+public class DecisionMaking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 8, y = 7;
+		int a = 10;
+		int b = 11;
+		
+		if(x >= y) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+		
+		if(!(a<b) || (a==b)) {
+			System.out.println("Condition True.");
+		} else {
+			System.out.println("Condition is False.");
+		}
+
+	}
+
+}
